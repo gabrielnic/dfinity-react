a9262c4a26d1a593654e8754fef09937d8753b46
gabriel <gabriel.nicola88@gmail.com>
2022-12-08 08:48:34 +0200

    blob branch

--- src/App.tsx
+++ src/App.tsx
@@ -66,10 +66,33 @@ function App() {
     });
 }
 
+const b64toUint8 = (b64Data: string): Uint8Array => {
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteArrays[i] = byteCharacters.charCodeAt(i);
+  }
+  return new Uint8Array(byteArrays);
+}
+
+
+const uploadBlob = async (e: React.FormEvent<HTMLButtonElement>) => {
+  e.preventDefault();
+  // test blob
+  const base64Blob = 'H4sIAAAAAAAACo2W2W7qSBCG61m4nUEYDAaONBfsELIBIRBG0VHYtwSchDWad5+v6mAgcyZSZLndSy1//V1d7Q8JSU8WPK/SlzdGP+RvCUtU/jy0jjzShuRdJjKXwVEmJB2Zyq34ciMpiUlSCsg+yQO9uGylJZdSlaw0+GaQVhsLcU1mLCNpyg7ZpjzLCzamsqYdYWsqJeY2B42w3GG7hL0sVtPoNCXHaEC7ZJxhfAWKK3R/aVSlje8FfnvILfA+o/+E7Jx+i9FYitjfSe2g0SF2n9UeGIqg8bA5kBWed3jxsLIFaQ0vAaqalLGUQeYJeZengIWF3IOiQPxTubbZyyOqOfgTjG5BHZU8ejO0y2CtYanIdwv/e2INuPJZKcB526T2YJuBx2PuAj7uka6B08NWoFFHLmM+ajCYkyHrPhZ9RtegK2NtBLbhUSOLxAbGxiY7AlEBZBX4GoJ2iO0hMzvYCDTyeOljawLmO/ayDapnZrKstPA7QLsCutaRKx9LJaJekUFx7Cm7VTjKI73D5wCektj5eeRqZ4xMyaocXE15NFdK7EEGFmLMF/i+ne2Hy/oaZB28ZNCagyzG3AvoNqa1hbUX4gr2fMrePIPnJ5762O+hUULmynJ0jVxbuuTM6IyrPn73MNnCUp0Vh2x10epiZYHFDdm6xWoQxxKGctj0QeGiE+eru+KzEx4ei4x22O8dUe2Id285uiU6j36T/Ugy20ArB+Y7xlHkA1QFvDvMdsC3oH9j7HbwEkaqAUsP2KqBOvDxgr89thLEE8eL7nkYyzX0qqyW7CQk0AvY3dg5GMFIg926ZW3GV/Nogu1XrKSw79huBue8iezMzqHmbZ13gu0OsRR5mvQneOgf47jn8fEQ5lshnjEyE1BVsJqwLFiZ/PMxjixW88z3YOTeMrRgZ3qFlyfQJCzyWzwGqNbI5ZF8AUEZT1PL9hzxdYk8Y98xPkbHyFcg8OC0y/eCSNPYbTDeYF1PsAcHV4xPubsh4rRlRA9PDzDt4GGPzzmyNcu3GF4mR40ZM1E8DcB0h3bf8spBXquXD7MJLA6s3gS15AYsLjNv2I4xnjFTsNOXssrrwZPuYRBHgmj3vAP0ruCtC7u6Ny3OpEvsr1axL3lP1WcJj2M7NVHalVXRNV70bE3ZFx8d5TrQiGG9jVetdCO8PSOrVTqL5IB+A19j2ymN/PFwQl5BsbR6+M6rN8sH7RORvSH5fnbbrLCtlX1LNC2rjHpi+8xP4U3zyoPBLFIBIg90P43HgeX62HK0AYYi7QXWk0gU8HGqI00i29gJv0SqZH56SFT4XjBfZ74NY5vfYmhYBO9nN+TH2WoFBDobgs0kfjzaJG2aftxOUJoVhzZGfM4Bz4mHQN+x+a31w+i5dkdr5TjN6Hh/kP6H0f/jSJluHAx6ezv0tbImaBWdPg69r3BEP+GIGfpzHBpL6ptINGLvwEESybixoK/OKRLl6HtIEr+hcL6JImoM6J4kDMMvJDpyjQnX7pSvUMQ+oXDB/ZmPxGF8wvFoWELon3Jf87yMV823zH+eHJnukukrcrDKqEauZ8lRx6r8ym4qreBxIkvCXgSPfbI2b/8U+mdyj06HlQZW1qzlecbI6m27tJNVwPbX75AaMKSipgzFwPZnC6YhT4dal7K6UuPUaAV/I9Y052iDpypyeyTzll11pJbUhwcQqr0tZ1Rva/336vJcI5u0KqUYH+zOb2BjxtmL4NlBZ4GO7lgEvR3SOXzpjaZ/ixHbrTI4RiCsozG3KjZi5RZr+t81Y6w6envr7ebauY0RxwSpNSt/2G3cIAOUxx4+uvYn2GdlYX9qejtk7a7uo+HZ302ENkON9YyryGHngj38iydkO/8vfGMxh3gLAAA=';
+  const ba = await BackendActor.getBackendActor();
+  const append = await ba.appendData('1', { name: 'test1', data: b64toUint8(base64Blob) });
+  console.log(append);
+
+}
+
+
+
 
   return (
     <div className="App">
       <header className="App-header">
+      <button onClick={uploadBlob}>Append</button>
         <h2>Value received from Dfinity: {val}</h2>
         {!logged && 
           <div>
--- src/test/main.mo
+++ src/test/main.mo
@@ -6,11 +6,38 @@ import Text "mo:base/Text";
 import Char "mo:base/Char";
 import Nat32 "mo:base/Nat32";
 import Nat "mo:base/Nat";
+import Blob "mo:base/Blob";
+import TrieMap "mo:base/TrieMap";
 import Debug "mo:base/Debug";
 import Prim "mo:prim";
 
 shared ({caller = owner}) actor class Test() = this {
   stable var currentValue: Nat = 1;
+
+  type BlobTest = {
+    data : Blob;
+    name : Text;
+  };
+  let data = TrieMap.TrieMap<Text, BlobTest>(Text.equal, Text.hash);
+
+
+  public shared ({ caller }) func appendData(key: Text, bt: BlobTest) : async () {
+    Debug.print("append key" # key);
+    let _ = data.put(key, bt);
+
+  };
+
+  public shared query ({ caller })  func getData(key: Text) : async ?Blob {
+    let d = data.get(key);
+    switch(d) {
+      case null { return null; };
+      case (?d) {
+        Debug.print(debug_show(d.data));
+        return ?d.data;
+      };
+    };
+    return null;
+  };
 
   public shared (msg) func increment(): async Nat {
     currentValue += 1;
